b0f0fc2a803bf8647f139eca60a4adb506038889
naelsondouglas <naelson17@gmail.com>
2018-01-21 07:20:54 -0300

    created a script(install_requiriments.jl) to install all requirimentsand created a file to store the default kernels. At this moment the only one there is blur()

--- src/simple_ips.jl
+++ src/simple_ips.jl
@@ -4,6 +4,10 @@ using ImageMagick
 using TestImages
 using FileIO
 using ParallelAccelerator
+include("default_kernels.jl")
+
+
+
 
 using DocOpt
 using Images
@@ -20,19 +24,7 @@ macro getname(arg)
            string(arg)
        end
 
-@acc function blur(img, iterations)
-    buf = copy(img)
-    runStencil(buf, img, iterations, :oob_src_zero) do b, a
-       b[0,0] =
-      (a[-2,-2] * 0.003  + a[-1,-2] * 0.0133 + a[0,-2] * 0.0219 + a[1,-2] * 0.0133 + a[2,-2] * 0.0030 +
-       a[-2,-1] * 0.0133 + a[-1,-1] * 0.0596 + a[0,-1] * 0.0983 + a[1,-1] * 0.0596 + a[2,-1] * 0.0133 +
-       a[-2, 0] * 0.0219 + a[-1, 0] * 0.0983 + a[0, 0] * 0.1621 + a[1, 0] * 0.0983 + a[2, 0] * 0.0219 +
-       a[-2, 1] * 0.0133 + a[-1, 1] * 0.0596 + a[0, 1] * 0.0983 + a[1, 1] * 0.0596 + a[2, 1] * 0.0133 +
-       a[-2, 2] * 0.003  + a[-1, 2] * 0.0133 + a[0, 2] * 0.0219 + a[1, 2] * 0.0133 + a[2, 2] * 0.0030)
-    return a, b
-    end
-    return img
-end
+
     
 
 function process(session_id,kernel,input, iterations)
@@ -96,9 +88,7 @@ process(1,blur,"portinari.png",1)
 	return meta_csv
  end
 
-function loadDataset(infra_id::AbstractString,dataset::AbstractString)
-		run(`sudo docker cp $infra_id:/opt/datasets/$dataset  .`)
-end
+
 
 
 function getDatasetMetadata(name::String)
